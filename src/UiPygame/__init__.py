8a819f242be8b1a7883a5a7f7414675243479a85
Lucas Raulier <55331957+Lefantastique59@users.noreply.github.com>
2019-10-05 22:10:50 +0200

    Changement pour futures adaptations graphiques
    Ajout d'une fonction draw dans le bouton pour ne pas avoir a re-instancier le bouton a chaque dessin

--- src/UiPygame/__init__.py
+++ src/UiPygame/__init__.py
@@ -2,7 +2,7 @@ import pygame
 from pygame.locals import *
 
 class Bouton(object):
-    def __init__(self,frame,x,y,width,height,rgb,text,rgb_text,police):
+    def __init__(self,x,y,width,height,rgb):
         #Constructeur
         self.btn = pygame.Surface((width,height))
         self.btn.fill(rgb)
@@ -10,15 +10,16 @@ class Bouton(object):
         self.y = y
         self.width = width
         self.height = height
-        pygame.draw.rect(self.btn,rgb,self.btn.get_rect(),1)
-        text = police.render(text,True,rgb_text)
-        self.btn.blit(text,(width/2 - text.get_width()/2,height/2 - text.get_height()/2))
-        frame.blit(self.btn,(x,y))
 
+
+    def draw(self,frame,text,rgb_text,police):
+        #pygame.draw.rect(self.btn,rgb,self.btn.get_rect(),1)
+        text = police.render(text,True,rgb_text)
+        self.btn.blit(text,(self.width/2 - text.get_width()/2,self.height/2 - text.get_height()/2))
+        frame.blit(self.btn,(self.x,self.y))
 
     def isCursorInRange(self):
         pos = pygame.mouse.get_pos()
         if((self.x+self.width > pos[0] > self.x)and(self.y+self.height > pos[1] > self.y)):
             return True
         return False
-    

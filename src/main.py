f7e75ec296a9c502eeb90e33ccb3da5ff5ab5613
Lucas Raulier <55331957+Lefantastique59@users.noreply.github.com>
2019-10-06 03:50:07 +0200

    Changement structure qui gère les boutons
    La classe Data contient tout ce qui sera utilisé plusieurs fois tout au long du programme, si vous voulez changer l'état du jeu, il faudra avoir accès a data, cette classe a été créée dans le but de pouvoir changer l'état du jeu depuis une classe Partie et pour pouvoir accéder a diverse données dans le futur si besoin est.
    
    La variable état défini quel boutons réagissent a la souris et quel events de pygame sont surveillés a quel moment du jeu

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-from tkinter import *
 import pygame
 from pygame.locals import *
 import Sudoku
@@ -11,38 +10,53 @@ frame = pygame.display.set_mode((640,480))
 fond = pygame.image.load(os.path.join(os.path.dirname(os.path.abspath(__file__)), "assets", "background_menu.jpg")).convert()
 frame.blit(fond, (0,0))
 
-#cette variable représente les différents états dans lequel le jeu peux se trouver.
-#
-# 0 - Menu principal
-# 1 - options
-# 2 - demande si on reprend la grille du Sudoku
-# 3 - Sudoku
-# 4 -
-#
-etat = 0;
+class Data:
 
+    def __init__(self, frame):
+        # Cette variable représente les différents états dans lequel le jeu peut se trouver.
+        #
+        # 0 - Menu principal
+        # 1 - options
+        # 2 - demande si on reprend la grille du Sudoku
+        # 3 - choix du niveau de difficulté au Sudoku (peut être généralisé si tout les mini-jeux possèdent exactement 3 niveaux de difficulté)
+        # 4 - En partie de Sudoku
+        # 5 - Gain/Perte de la partie du Sudoku
+        #
+        self.etat = 0
+
+        # Cette variable représente la partie au cas ou on en aurait besoin.
+        self.partie = None;
+
+    # Fonction setEtat(e)
+    #
+    # e : le nouvel état du jeu
+    #
+    def setEtat(self, e):
+        self.etat = e
+
+data = Data(frame)
 police = pygame.font.SysFont('Impact',25)
 boutons = [];
 
 def createBoutons(tab):
     tab[:] = []
-    tab.append(ui.Bouton(30,30,150,50,(180,180,180)))
-    tab.append(ui.Bouton(30,100,150,50,(180,180,180)))
-    tab.append(ui.Bouton(30,160,150,50,(180,180,180)))
-    tab.append(ui.Bouton(30,220,150,50,(180,180,180)))
+    tab.append(ui.Bouton(30,30,150,50))
+    tab.append(ui.Bouton(30,100,150,50))
+    tab.append(ui.Bouton(30,160,150,50))
+    tab.append(ui.Bouton(30,220,150,50))
 
-    tab.append(ui.Bouton(300,160,150,50,(180,180,180)))
-    tab.append(ui.Bouton(300,220,150,50,(180,180,180)))
+    tab.append(ui.Bouton(300,160,150,50))
+    tab.append(ui.Bouton(300,220,150,50))
 
 
 
 def drawBoutons(frame, police):
-    boutons[0].draw(frame,"Sudoku",(255,255,255),police)
-    boutons[1].draw(frame,"Loto",(255,255,255),police)
-    boutons[2].draw(frame,"Bataille navale",(255,255,255),police)
-    boutons[3].draw(frame,"Poker",(255,255,255),police)
-    boutons[4].draw(frame,"Options",(255,255,255),police)
-    boutons[5].draw(frame,"Profil",(255,255,255),police)
+    boutons[0].draw(frame,(180,180,180),"Sudoku",(255,255,255),police)
+    boutons[1].draw(frame,(180,180,180),"Loto",(255,255,255),police)
+    boutons[2].draw(frame,(180,180,180),"Bataille navale",(255,255,255),police)
+    boutons[3].draw(frame,(180,180,180),"Poker",(255,255,255),police)
+    boutons[4].draw(frame,(180,180,180),"Options",(255,255,255),police)
+    boutons[5].draw(frame,(180,180,180),"Profil",(255,255,255),police)
 
 createBoutons(boutons)
 drawBoutons(frame, police)
@@ -53,10 +67,10 @@ while(not(fin)):
     for event in pygame.event.get():
         if(event.type == QUIT):
             fin = True
-        if(event.type == MOUSEBUTTONDOWN):
-            if (etat == 0):
+        elif(event.type == MOUSEBUTTONDOWN):
+            if (data.etat == 0):
                 if(boutons[0].isCursorInRange()):
-                    Sudoku.PartieG()
+                    data.partie = Sudoku.PartieG(frame, boutons, data)
                 elif(boutons[1].isCursorInRange()):
                     print("Lancement du loto")
                 elif(boutons[2].isCursorInRange()):
@@ -67,10 +81,34 @@ while(not(fin)):
                     print("Lancement des options")
                 elif(boutons[5].isCursorInRange()):
                     print("Lancement du profil")
-            elif (etat == 1):
+            elif (data.etat == 1):
+                print("OPTIONS")
+            elif (data.etat == 2):
                 if (boutons[0].isCursorInRange()):
-                    print("YOLO")
+                    data.partie.charger_grille()
                 elif (boutons[1].isCursorInRange()):
-                    print("YOLO 2")
+                    data.partie.choixniveau_gui(frame, boutons)
+            elif (data.etat == 3):
+                if (boutons[0].isCursorInRange()):
+                    data.partie.creerGrille(1)
+                elif (boutons[1].isCursorInRange()):
+                    data.partie.creerGrille(2)
+                elif (boutons[2].isCursorInRange()):
+                    data.partie.creerGrille(3)
+
+                data.setEtat(4);
+
+                # L'état de la partie passe à "En cours"
+                data.partie.etat_partie = 1;
+                data.partie.draw(frame)
+            elif (data.etat == 4):
+                pos = pygame.mouse.get_pos()
+                data.partie.grille_jeu.selectCase(pos[0], pos[1])
+                data.partie.draw(frame)
+        elif (event.type == MOUSEMOTION):
+            if (data.etat == 4):
+                pos = pygame.mouse.get_pos()
+                data.partie.grille_jeu.hoverCase(pos[0], pos[1])
+                data.partie.draw(frame)
 
 pygame.quit()

936fe19658cfef23a6c50d39d804b9aa7cb4480a
bverhul <benjaminverhulst@live.fr>
2019-10-11 11:45:07 +0200

    Ajout d'une classe Title pour mettre un titre dans une fenetre

--- src/UiPygame/__init__.py
+++ src/UiPygame/__init__.py
@@ -20,7 +20,7 @@ class Bouton(object):
             self.police = pygame.font.SysFont('Impact',14)
 
     #Permet de dessiner le bouton dans une fenetre donnée
-    def draw(self,frame,rgb=0,text=0,rgb_text=0,police=0):
+    def draw(self,frame):
         if(self.isCursorInRange() and self.rgb_when_change != None):
             self.btn.fill(self.rgb_when_change)
         else:
@@ -44,7 +44,7 @@ class Bouton(object):
 
 #A ne pas utiliser, en construction xD
 class Title():
-    def __init__(self,x,y,width,height,border = 0,rgb_when_change = (0,0,0),text = "",rgb=(255,255,255),police = None,rgb_text = (0,0,0)):
+    def __init__(self,x,y,width,height,border = 0,text = "",rgb=(255,255,255),police = None,rgb_text = (0,0,0)):
         #Constructeur
         self.btn = pygame.Surface((width,height))
         self.x = x
@@ -52,7 +52,6 @@ class Title():
         self.width = width
         self.height = height
         self.border = border
-        self.rgb_when_change = rgb_when_change
         self.text = text
         self.rgb = rgb
         self.rgb_text = rgb_text
@@ -62,27 +61,14 @@ class Title():
             self.police = pygame.font.SysFont('Impact',14)
 
     #Permet de dessiner le bouton dans une fenetre donnée
-    def draw(self,frame,rgb=0,text=0,rgb_text=0,police=0):
-        if(self.isCursorInRange() and self.rgb_when_change != None):
-            self.btn.fill(self.rgb_when_change)
-        else:
-            self.btn.fill(self.rgb)
+    def draw(self,frame):
+        self.btn.fill(self.rgb)
         if(self.border > 0):
             pygame.draw.rect(self.btn,(0,0,0),[0,0,self.width,self.height],self.border)
         text_on = self.police.render(self.text,True,self.rgb_text)
         self.btn.blit(text_on,(self.width/2 - text_on.get_width()/2,self.height/2 - text_on.get_height()/2))
         frame.blit(self.btn,(self.x,self.y))
 
-    #Détermine si la souris est dans la surface du bouton
-    def isCursorInRange(self):
-        pos = pygame.mouse.get_pos()
-        if((self.x+self.width > pos[0] > self.x)and(self.y+self.height > pos[1] > self.y)):
-            return True
-        return False
-
-    def update(self,frame):
-        self.draw(frame)
-        pygame.display.flip()
 
 
     

dcfde59d7935711020cb0265b98658bc14791812
bverhul <benjaminverhulst@live.fr>
2019-10-11 11:33:59 +0200

    Classe bouton : modifications du constructeur et draw(), main modifié

--- src/UiPygame/__init__.py
+++ src/UiPygame/__init__.py
@@ -2,24 +2,87 @@ import pygame
 from pygame.locals import *
 
 class Bouton(object):
-    def __init__(self,x,y,width,height):
+    def __init__(self,x,y,width,height,border = 0,rgb_when_change = (0,0,0),text = "",rgb=(255,255,255),police = None,rgb_text = (0,0,0)):
         #Constructeur
         self.btn = pygame.Surface((width,height))
         self.x = x
         self.y = y
         self.width = width
         self.height = height
+        self.border = border
+        self.rgb_when_change = rgb_when_change
+        self.text = text
+        self.rgb = rgb
+        self.rgb_text = rgb_text
+        if(police != None):
+            self.police = police
+        else:
+            self.police = pygame.font.SysFont('Impact',14)
 
-
-    def draw(self,frame,rgb,text,rgb_text,police):
-        #pygame.draw.rect(self.btn,rgb,self.btn.get_rect(),1)
-        self.btn.fill(rgb)
-        text = police.render(text,True,rgb_text)
-        self.btn.blit(text,(self.width/2 - text.get_width()/2,self.height/2 - text.get_height()/2))
+    #Permet de dessiner le bouton dans une fenetre donnée
+    def draw(self,frame,rgb=0,text=0,rgb_text=0,police=0):
+        if(self.isCursorInRange() and self.rgb_when_change != None):
+            self.btn.fill(self.rgb_when_change)
+        else:
+            self.btn.fill(self.rgb)
+        if(self.border > 0):
+            pygame.draw.rect(self.btn,(0,0,0),[0,0,self.width,self.height],self.border)
+        text_on = self.police.render(self.text,True,self.rgb_text)
+        self.btn.blit(text_on,(self.width/2 - text_on.get_width()/2,self.height/2 - text_on.get_height()/2))
         frame.blit(self.btn,(self.x,self.y))
 
+    #Détermine si la souris est dans la surface du bouton
     def isCursorInRange(self):
         pos = pygame.mouse.get_pos()
         if((self.x+self.width > pos[0] > self.x)and(self.y+self.height > pos[1] > self.y)):
             return True
         return False
+
+    def update(self,frame):
+        self.draw(frame)
+        pygame.display.flip()
+
+#A ne pas utiliser, en construction xD
+class Title():
+    def __init__(self,x,y,width,height,border = 0,rgb_when_change = (0,0,0),text = "",rgb=(255,255,255),police = None,rgb_text = (0,0,0)):
+        #Constructeur
+        self.btn = pygame.Surface((width,height))
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.border = border
+        self.rgb_when_change = rgb_when_change
+        self.text = text
+        self.rgb = rgb
+        self.rgb_text = rgb_text
+        if(police != None):
+            self.police = police
+        else:
+            self.police = pygame.font.SysFont('Impact',14)
+
+    #Permet de dessiner le bouton dans une fenetre donnée
+    def draw(self,frame,rgb=0,text=0,rgb_text=0,police=0):
+        if(self.isCursorInRange() and self.rgb_when_change != None):
+            self.btn.fill(self.rgb_when_change)
+        else:
+            self.btn.fill(self.rgb)
+        if(self.border > 0):
+            pygame.draw.rect(self.btn,(0,0,0),[0,0,self.width,self.height],self.border)
+        text_on = self.police.render(self.text,True,self.rgb_text)
+        self.btn.blit(text_on,(self.width/2 - text_on.get_width()/2,self.height/2 - text_on.get_height()/2))
+        frame.blit(self.btn,(self.x,self.y))
+
+    #Détermine si la souris est dans la surface du bouton
+    def isCursorInRange(self):
+        pos = pygame.mouse.get_pos()
+        if((self.x+self.width > pos[0] > self.x)and(self.y+self.height > pos[1] > self.y)):
+            return True
+        return False
+
+    def update(self,frame):
+        self.draw(frame)
+        pygame.display.flip()
+
+
+    
--- src/main.py
+++ src/main.py
@@ -2,113 +2,117 @@ import pygame
 from pygame.locals import *
 import Sudoku
 import UiPygame as ui
-import os
+import os,sys
 
-pygame.init()
-frame = pygame.display.set_mode((640,480))
+try:
+    pygame.init()
+    frame = pygame.display.set_mode((640,480))
 
-fond = pygame.image.load(os.path.join(os.path.dirname(os.path.abspath(__file__)), "assets", "background_menu.jpg")).convert()
-frame.blit(fond, (0,0))
+    fond = pygame.image.load(os.path.join(os.path.dirname(os.path.abspath(__file__)), "assets", "background_menu.jpg")).convert()
+    frame.blit(fond, (0,0))
 
-class Data:
+    class Data:
 
-    def __init__(self, frame):
-        # Cette variable représente les différents états dans lequel le jeu peut se trouver.
+        def __init__(self, frame):
+            # Cette variable représente les différents états dans lequel le jeu peut se trouver.
+            #
+            # 0 - Menu principal
+            # 1 - options
+            # 2 - demande si on reprend la grille du Sudoku
+            # 3 - choix du niveau de difficulté au Sudoku (peut être généralisé si tout les mini-jeux possèdent exactement 3 niveaux de difficulté)
+            # 4 - En partie de Sudoku
+            # 5 - Gain/Perte de la partie du Sudoku
+            #
+            self.etat = 0
+
+            # Cette variable représente la partie au cas ou on en aurait besoin.
+            self.partie = None;
+
+        # Fonction setEtat(e)
         #
-        # 0 - Menu principal
-        # 1 - options
-        # 2 - demande si on reprend la grille du Sudoku
-        # 3 - choix du niveau de difficulté au Sudoku (peut être généralisé si tout les mini-jeux possèdent exactement 3 niveaux de difficulté)
-        # 4 - En partie de Sudoku
-        # 5 - Gain/Perte de la partie du Sudoku
+        # e : le nouvel état du jeu
         #
-        self.etat = 0
+        def setEtat(self, e):
+            self.etat = e
 
-        # Cette variable représente la partie au cas ou on en aurait besoin.
-        self.partie = None;
+    data = Data(frame)
+    police = pygame.font.SysFont('Impact',25)
+    color_boutons = (180,180,180)
+    color_boutons_change = (45,45,45)
+    
+    boutons = []
+    boutons.append(ui.Bouton(30,30,150,50,2,color_boutons_change,"Sudoku",color_boutons,police,(255,255,255)))
+    boutons.append(ui.Bouton(30,100,150,50,2,color_boutons_change,"Loto",color_boutons,police,(255,255,255)))
+    boutons.append(ui.Bouton(30,160,150,50,2,color_boutons_change,"Bataille navale",color_boutons,police,(255,255,255)))
+    boutons.append(ui.Bouton(30,220,150,50,2,color_boutons_change,"Poker",color_boutons,police,(255,255,255)))
 
-    # Fonction setEtat(e)
-    #
-    # e : le nouvel état du jeu
-    #
-    def setEtat(self, e):
-        self.etat = e
-
-data = Data(frame)
-police = pygame.font.SysFont('Impact',25)
-boutons = [];
-
-def createBoutons(tab):
-    tab[:] = []
-    tab.append(ui.Bouton(30,30,150,50))
-    tab.append(ui.Bouton(30,100,150,50))
-    tab.append(ui.Bouton(30,160,150,50))
-    tab.append(ui.Bouton(30,220,150,50))
-
-    tab.append(ui.Bouton(300,160,150,50))
-    tab.append(ui.Bouton(300,220,150,50))
+    boutons.append(ui.Bouton(300,160,150,50,2,color_boutons_change,"Options",color_boutons,police,(255,255,255)))
+    boutons.append(ui.Bouton(300,220,150,50,2,color_boutons_change,"Profil",color_boutons,police,(255,255,255)))
 
 
+    for i in boutons:
+        i.draw(frame)
 
-def drawBoutons(frame, police):
-    boutons[0].draw(frame,(180,180,180),"Sudoku",(255,255,255),police)
-    boutons[1].draw(frame,(180,180,180),"Loto",(255,255,255),police)
-    boutons[2].draw(frame,(180,180,180),"Bataille navale",(255,255,255),police)
-    boutons[3].draw(frame,(180,180,180),"Poker",(255,255,255),police)
-    boutons[4].draw(frame,(180,180,180),"Options",(255,255,255),police)
-    boutons[5].draw(frame,(180,180,180),"Profil",(255,255,255),police)
+    pygame.display.flip()
 
-createBoutons(boutons)
-drawBoutons(frame, police)
-pygame.display.flip()
+    fin = False
+    while(not(fin)):
+        for event in pygame.event.get():
+            if(event.type == QUIT):
+                fin = True
+            elif(event.type == MOUSEBUTTONDOWN):
+                if (data.etat == 0):
+                    if(boutons[0].isCursorInRange()):
+                        data.partie = Sudoku.PartieG(frame, boutons, data)
+                    elif(boutons[1].isCursorInRange()):
+                        print("Lancement du loto")
+                    elif(boutons[2].isCursorInRange()):
+                        print("Lancement de la bataille navale")
+                    elif(boutons[3].isCursorInRange()):
+                        print("Lancement du poker")
+                    elif(boutons[4].isCursorInRange()):
+                        print("Lancement des options")
+                    elif(boutons[5].isCursorInRange()):
+                        print("Lancement du profil")
+                elif (data.etat == 1):
+                    print("OPTIONS")
+                elif (data.etat == 2):
+                    if (boutons[0].isCursorInRange()):
+                        data.partie.charger_grille()
+                    elif (boutons[1].isCursorInRange()):
+                        data.partie.choixniveau_gui(frame, boutons)
+                elif (data.etat == 3):
+                    if (boutons[0].isCursorInRange()):
+                        data.partie.creerGrille(1)
+                    elif (boutons[1].isCursorInRange()):
+                        data.partie.creerGrille(2)
+                    elif (boutons[2].isCursorInRange()):
+                        data.partie.creerGrille(3)
 
-fin = False
-while(not(fin)):
-    for event in pygame.event.get():
-        if(event.type == QUIT):
-            fin = True
-        elif(event.type == MOUSEBUTTONDOWN):
-            if (data.etat == 0):
-                if(boutons[0].isCursorInRange()):
-                    data.partie = Sudoku.PartieG(frame, boutons, data)
-                elif(boutons[1].isCursorInRange()):
-                    print("Lancement du loto")
-                elif(boutons[2].isCursorInRange()):
-                    print("Lancement de la bataille navale")
-                elif(boutons[3].isCursorInRange()):
-                    print("Lancement du poker")
-                elif(boutons[4].isCursorInRange()):
-                    print("Lancement des options")
-                elif(boutons[5].isCursorInRange()):
-                    print("Lancement du profil")
-            elif (data.etat == 1):
-                print("OPTIONS")
-            elif (data.etat == 2):
-                if (boutons[0].isCursorInRange()):
-                    data.partie.charger_grille()
-                elif (boutons[1].isCursorInRange()):
-                    data.partie.choixniveau_gui(frame, boutons)
-            elif (data.etat == 3):
-                if (boutons[0].isCursorInRange()):
-                    data.partie.creerGrille(1)
-                elif (boutons[1].isCursorInRange()):
-                    data.partie.creerGrille(2)
-                elif (boutons[2].isCursorInRange()):
-                    data.partie.creerGrille(3)
+                    if(boutons[0].isCursorInRange() or boutons[1].isCursorInRange() or boutons[2].isCursorInRange()):
+                        data.setEtat(4);
+                
+                        # L'état de la partie passe à "En cours"
+                        data.partie.etat_partie = 1;
+                        data.partie.draw(frame)
+                elif (data.etat == 4):
+                    pos = pygame.mouse.get_pos()
+                    data.partie.grille_jeu.selectCase(pos[0], pos[1])
+                    data.partie.draw(frame)
+            elif (event.type == MOUSEMOTION):
+                if (data.etat == 4):
+                    pos = pygame.mouse.get_pos()
+                    data.partie.grille_jeu.hoverCase(pos[0], pos[1])
+                    data.partie.draw(frame)
 
-                data.setEtat(4);
-
-                # L'état de la partie passe à "En cours"
-                data.partie.etat_partie = 1;
-                data.partie.draw(frame)
-            elif (data.etat == 4):
-                pos = pygame.mouse.get_pos()
-                data.partie.grille_jeu.selectCase(pos[0], pos[1])
-                data.partie.draw(frame)
-        elif (event.type == MOUSEMOTION):
-            if (data.etat == 4):
-                pos = pygame.mouse.get_pos()
-                data.partie.grille_jeu.hoverCase(pos[0], pos[1])
-                data.partie.draw(frame)
-
+                else:
+                    for b in boutons:
+                        b.update(frame)
+                    
+except Exception as e:
+    pygame.quit()
+    exc_type, exc_obj, exc_tb = sys.exc_info()
+    fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+    print("Type : ",exc_type," dans ",fname," ligne : ",exc_tb.tb_lineno)
+    print(e)
 pygame.quit()

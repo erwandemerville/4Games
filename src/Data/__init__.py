7100bf4aafb7326a9efec7d9041f179b5ec1551c
Lucas Raulier <55331957+Lefantastique59@users.noreply.github.com>
2019-10-25 06:33:17 +0200

    ajout du système de particules

--- src/Data/__init__.py
+++ src/Data/__init__.py
@@ -3,11 +3,12 @@ from pygame.locals import *
 import SubMenu as sb
 import UiPygame as ui
 import os
+from Particules import ParticleSystem
 
 class Data:
 
         menus = []
-    
+
         def __init__(self, frame):
             # Cette variable représente les différents états dans lequel le jeu peut se trouver.
             #
@@ -30,6 +31,7 @@ class Data:
             self.fin = False
             self.fond = pygame.image.load(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../assets", "background_menu.jpg")).convert()
             frame.blit(self.fond, (0,0))
+            self.particules = ParticleSystem.ParticleSystem()
 
         @staticmethod
         def init(frame, data):
@@ -44,7 +46,7 @@ class Data:
                         ui.Bouton(50,frame.get_height()-60,150,50,2,color_boutons_change,"Options",color_boutons,police,color_f),
                         ui.Bouton((frame.get_width()/2)-(150/2),frame.get_height()-60,150,50,2,color_boutons_change,"Profil",color_boutons,police,color_f),
                         ui.Bouton(frame.get_width()-200,frame.get_height()-60,150,50,2,color_boutons_change,"Quitter",color_boutons,police,color_f)]))
-           
+
             Data.menus.append(sb.Menu_Optn(data,
                                            [    ui.Bouton(50,frame.get_height()-60,200,50,2,color_boutons_change,"Sauvegarder",color_boutons,police,color_f),
                                                 ui.Bouton(frame.get_width()-50-200,frame.get_height()-60,200,50,2,color_boutons_change,"Retour",color_boutons,police,color_f),
@@ -63,7 +65,7 @@ class Data:
             Data.menus.append(sb.Menu_Sudoku(data, [ui.Bouton(480,420,150,50,2,color_boutons_change,"Pause",(152,152,152),police,(255,255,255))]))
 
             Data.menus.append(sb.Menu_SudokuEnd(data, [ui.Bouton(245, 200, 150, 50,2,(2,235,2),"Recommencer",color_boutons,police20,(255,255,255)), ui.Bouton(245, 300, 150, 50,2,(220,220,2),"Retourner au menu",color_boutons,pygame.font.SysFont('Impact',18),(255,255,255))]))
-        
+
             Data.menus.append(sb.Menu_SudokuP(data, [ui.Bouton(180,165,frame.get_width()-300-60,50,2,(120,120,120),"Reprendre",(152,152,152),police,(255,255,255)),
                                                      ui.Bouton(180,240,frame.get_width()-300-60,50,2,(120,120,120),"Sauvegarder et quitter",(152,152,152),police,(255,255,255)),
                                                      ui.Bouton(180,315,frame.get_width()-300-60,50,2,(120,120,120),"Quitter sans sauvegarder",(152,152,152),police,(255,255,255))]))
@@ -90,3 +92,10 @@ class Data:
         #
         def setMusic(self,e):
             self.music_active = e
+
+        # Fonction getParticleSystem(self)
+        #
+        # retourne le system de particules
+        #
+        def getParticleSystem(self):
+            return self.particules

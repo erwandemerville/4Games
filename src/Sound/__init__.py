ef5e734f78e4f1a9b64ce68b2a29934da00aa3dd
bverhul <benjaminverhulst@live.fr>
2019-11-16 15:40:32 +0100

    Ajout des commentaires pour toute la classe SoundManager

--- src/Sound/__init__.py
+++ src/Sound/__init__.py
@@ -1,8 +1,12 @@
 import pygame
 from pygame.mixer import Sound
+# Classe réalisée pour l'algorithmique et développement - L3
+# Réalisation par BendoTV - 2019
 
-
+# Classe gérant l'ensemble des sons de l'application
 class SoundManager():
+# un "sound" : son durant moins de 10s
+# une "music" : son durant au moins 10s
 
     def __init__(self,data):
         self.data = data
@@ -11,21 +15,24 @@ class SoundManager():
                            "byebye": Sound("assets/Au revoir.wav")}
         self.music_link = {"triste": Sound("assets/tristitude.wav")}
 
+    # Joue un son avec sound, un élement de sound_link
     def playSound(self,sound):
         if(self.data.sound_active):
             son = self.sound_link[sound]
             son.set_volume(1)
             son.play()
+    # Arrête un son avec sound, un élement de sound_link
     def stopSound(self,sound):
         if(self.data.sound_active):
             son = self.sound_link[sound]
             son.stop()
-
+    # Joue une musique avec music, un élement de music_link
     def playMusic(self,music):
         if(self.data.music_active):
             son = self.music_link[music]
             son.set_volume(1)
             son.play()
+    # Arrête une musique avec music, un élement de music_link
     def stopMusic(self,music):
         if(self.data.music_active):
             son = self.music_link[music]
